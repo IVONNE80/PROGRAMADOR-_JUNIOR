29f418fc87a4398a5b452288f58ed5128b75cf35
esc_codigo04 <esc_codigo04@test.com>
2022-10-27 12:04:31 -0500

    java

---
+++ variables/vv/src/Local.java
@@ -0,0 +1,22 @@
+public class Local {
+   public static void main(String[] args) {
+    /*Variables locales */
+    //Declaramos la variable
+    String h;
+    // inicializamos la variable
+    h = "Variables locales";
+    System.out.println(h);
+
+    // Instanciar objeto de tipo local
+
+    Local v1 = new Local();
+    // Objeto v1 y el metodo saludo ()
+    String neme = v1.mayusculas(nombre:"ivonne");
+    System.out.println("nombre: " + neme);
+}
+public void saludo (String nombre) {//nombre
+    // el objeto nombre y metodo length()
+    System.out.printl(nombre.len)
+    }
+    
+
