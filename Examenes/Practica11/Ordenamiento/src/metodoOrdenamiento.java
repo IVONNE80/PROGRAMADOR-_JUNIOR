66fc49ef2c88811765b5c9a32a5eba42311b1389
esc_cod4 <esc_cod4@test.com>
2022-12-02 11:46:52 -0600

    l

--- Examenes/Practica11/Ordenamiento/src/metodoOrdenamiento.java
+++ Examenes/Practica11/Ordenamiento/src/metodoOrdenamiento.java
@@ -1,54 +1,38 @@
-import java.util.Arrays;
-
-public class metodoOrdenamiento {
+public class MetodoOrdenamiento {
 
     public void imprimeArreglo(String mensaje, int[] arreglo) {
-        int listas[] = { 11, 23, 9, 20 };
-        System.out.println(Arrays.toString(listas));
+        System.out.println(mensaje);
+        for (int i : arreglo){
+            System.out.print(i+" ");
+        }
     }
 
     public void saltoLinea() {
-
         System.out.println("\n");
-        /* */
     }
 
     public static void main(String args[]) {
-
-        metodoOrdenamiento objOrdena = new metodoOrdenamiento();
-
+        int[] arreglo = { 11, 23, 9, 20};
+        MetodoOrdenamiento objOrdena = new MetodoOrdenamiento();
         objOrdena.imprimeArreglo("Arreglo inicial: ", arreglo);
-
         objOrdena.saltoLinea();
-
         for (int i = 0; i < arreglo.length; i++) {
-
             System.out.println("i = " + i);
-
             for (int j = 0; j < arreglo.length - 1; j++) {
-
-                objOrdena.imprimeArreglo("  Arreglo comparando: ", arreglo);
-
+                objOrdena.imprimeArreglo(" Arreglo comparando: ", arreglo);
                 System.out.println(" <----- j = " + j + " -- " + arreglo[j] + " > " + arreglo[j + 1] + "?");
-
                 if (arreglo[j] > arreglo[j + 1]) {
-
-                    System.out.println("    ------- Intercambia " + arreglo[j] + " con " + arreglo[j + 1]);
-                    /* */
-
-                    objOrdena.imprimeArreglo("    Arreglo modificado: ", arreglo);
-
+                    System.out.println(" ------- Intercambia " + arreglo[j] + " con " + arreglo[j + 1]);
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j + 1];
+                    arreglo[j + 1] = aux;
+                    
+                    objOrdena.imprimeArreglo(" Arreglo modificado: ", arreglo);
                     objOrdena.saltoLinea();
-
                 }
-
             }
-
         }
-
         objOrdena.saltoLinea();
-
         objOrdena.imprimeArreglo("Arreglo final: ", arreglo);
-
     }
 }
